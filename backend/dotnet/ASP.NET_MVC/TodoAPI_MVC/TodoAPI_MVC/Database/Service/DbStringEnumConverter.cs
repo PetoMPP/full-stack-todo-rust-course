29a5f855c52df5ea8e7ef147ee6cb03206acc2b6
PetoMPP <88336224+PetoMPP@users.noreply.github.com>
2022-12-30 18:19:11 +0100

    fix: fix Convert func of DbStringEnumConverter

--- backend/dotnet/ASP.NET_MVC/TodoAPI_MVC/TodoAPI_MVC/Database/Service/DbStringEnumConverter.cs
+++ backend/dotnet/ASP.NET_MVC/TodoAPI_MVC/TodoAPI_MVC/Database/Service/DbStringEnumConverter.cs
@@ -3,6 +3,6 @@
     public class DbStringEnumConverter : DbValueConverter
     {
         public override Func<Type, bool> CanConvert => t => t.IsEnum;
-        public override Func<object?, string> Convert => v => $"{v}";
+        public override Func<object?, string> Convert => v => $"'{v}'";
     }
 }
