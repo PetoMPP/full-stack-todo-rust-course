135318af0cf3803ca76600ef895a42688f1167a0
PetoMPP <88336224+PetoMPP@users.noreply.github.com>
2023-01-14 18:41:44 +0100

    chore: logout now happens on HTTP 401 and Json parsing

--- frontend/rust/yew/src/utils.rs
+++ frontend/rust/yew/src/utils.rs
@@ -4,24 +4,34 @@ use uuid::Uuid;
 use yew::UseStateHandle;
 use yewdux::prelude::Dispatch;
 
-use crate::{api::api_client::ApiError, components::{pages::error_data::ErrorData, organisms::error_message::DEFAULT_TIMEOUT_MS}, SessionStore};
+use crate::{
+    api::api_client::ApiError,
+    components::{organisms::error_message::DEFAULT_TIMEOUT_MS, pages::error_data::ErrorData},
+    SessionStore,
+};
 
 pub fn handle_api_error(
     error: ApiError,
     session_dispatch: Dispatch<SessionStore>,
-    error_data: Option<UseStateHandle<ErrorData>>)
-{
+    error_data: Option<UseStateHandle<ErrorData>>,
+) {
     log!(error.to_string());
-    // if let ApiError::HttpStatus(code, _) = error {
-    //     if code == 401u16 || code == 403u16 {
-    session_dispatch.reduce(|store| {
-        let mut store = store.deref().clone();
-        store.user = None;
-        store
-    });
-    //     }
-    // }
-    
+    if let ApiError::HttpStatus(code, _) = error {
+        if code == 401u16 {
+            session_dispatch.reduce(|store| {
+                let mut store = store.deref().clone();
+                store.user = None;
+                store
+            });
+        }
+    } else if let ApiError::Parse(_) = error {
+        session_dispatch.reduce(|store| {
+            let mut store = store.deref().clone();
+            store.user = None;
+            store
+        });
+    }
+
     match error_data {
         Some(error_data) => {
             let error_uuid = Uuid::new_v4();
@@ -36,8 +46,12 @@ pub fn handle_api_error(
             }
             error_data.set(ErrorData::default());
 
-            error_data.set(ErrorData { message: error.to_string(), display: true, uuid: error_uuid });
-        },
+            error_data.set(ErrorData {
+                message: error.to_string(),
+                display: true,
+                uuid: error_uuid,
+            });
+        }
         None => log!(error.to_string()),
     }
 }
