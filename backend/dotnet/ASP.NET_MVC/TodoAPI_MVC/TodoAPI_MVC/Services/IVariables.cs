24cad7951abad92c9b0d4922a47ee11357748612
PetoMPP <88336224+PetoMPP@users.noreply.github.com>
2023-01-29 20:48:49 +0100

    chore: use DB_HOST variable

--- backend/dotnet/ASP.NET_MVC/TodoAPI_MVC/TodoAPI_MVC/Services/IVariables.cs
+++ backend/dotnet/ASP.NET_MVC/TodoAPI_MVC/TodoAPI_MVC/Services/IVariables.cs
@@ -3,6 +3,7 @@
     public interface IVariables
     {
         public string DatabaseMode { get; set; }
+        public string DatabaseHost { get; set; }
         public string DatabaseUser { get; set; }
         public string DatabasePassword { get; set; }
         public string JwtSecret { get; set; }
